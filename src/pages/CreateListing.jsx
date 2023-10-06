21a261f692ce679e7fa67d4bd932a8f0fe3370cb
unknown <shanelwilliams2018@gmail.com>
2023-10-05 23:39:06 -0600

    started adding details to create a listing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,58 +1,198 @@
 import React, { useRef } from 'react'
-import { useState,useEffect } from 'react'
-import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import { useState, useEffect } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
-    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
-    const [loading, setLoading] =useState(false)
-    const [formData, setFormData] = useState({
-        type: 'rent',
-        name: '',
-        bedrooms: 1,
-        bathrooms: 1,
-        parking: false,
-        furnished: false,
-        address: '',
-        offer: false,
-        regularPrice: 0,
-        discounted: 0,
-        images: {},
-        latitude: 0,
-        longitude: 0,
-    })
+	const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+	const [loading, setLoading] = useState(false)
+	const [formData, setFormData] = useState({
+		type: 'rent',
+		name: '',
+		bedrooms: 1,
+		bathrooms: 1,
+		parking: false,
+		furnished: false,
+		address: '',
+		offer: false,
+		regularPrice: 0,
+		discountedPrice: 0,
+		images: {},
+		latitude: 0,
+		longitude: 0,
+	})
 
-    const auth = getAuth()
-    const navigate = useNavigate()
-    const isMounted = useRef(true)
+	const {
+		type,
+		name,
+		bedrooms,
+		bathrooms,
+		parking,
+		furnished,
+		address,
+		offer,
+		regularPrice,
+		discountedPrice,
+		images,
+		latitude,
+		longitude,
+	} = formData
 
-    useEffect(() => {
-        if(isMounted) {
-            onAuthStateChanged(auth, (user) => {
-                if(user) {
-                    setFormData({...formData, userRef: user.uid})
-                } else {
-                    navigate('/sign-in')
-                }
-            })
-        }
+	const auth = getAuth()
+	const navigate = useNavigate()
+	const isMounted = useRef(true)
 
-        return () => {
-            isMounted.current = false
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isMounted])
+	useEffect(() => {
+		if (isMounted) {
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					setFormData({ ...formData, userRef: user.uid })
+				} else {
+					navigate('/sign-in')
+				}
+			})
+		}
 
-    if(loading) {
-        return <Spinner /> 
-    }
+		return () => {
+			isMounted.current = false
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isMounted])
 
-  return (
-    <div>
-      Create
-    </div>
-  )
+	const onSubmit = (e) => {
+		e.preventDefault()
+	}
+
+	const onMutate = (e) => {}
+
+	if (loading) {
+		return <Spinner />
+	}
+
+	return (
+		<div className='profile'>
+			<header>
+				<p className='pageHeader'>Create a listing</p>
+			</header>
+
+			<main>
+				<form onSubmit={onSubmit}>
+					<label className='formLabel'>Sell / Rent</label>
+					<div className='formButtons'>
+						<button
+							type='button'
+							className={type === 'sale' ? 'formButtonActive' : 'formButton'}
+							id='type'
+							value='sale'
+							onClick={onMutate}
+						>
+							Sell
+						</button>
+						<button
+							type='button'
+							className={type === 'rent' ? 'formButtonActive' : 'formButton'}
+							id='type'
+							value='rent'
+							onClick={onMutate}
+						>
+							Rent
+						</button>
+					</div>
+
+					<label className='formLabel'>Name</label>
+					<input
+						className='formInputName'
+						type='text'
+						id='name'
+						value={name}
+						onChange={onMutate}
+						maxLength='32'
+						minLength='32'
+						required
+					/>
+
+					<div className='formRooms flex'>
+						<div>
+							<label className='formLabel'>Bedrooms</label>
+							<input
+								className='formInputSmall'
+								type='number'
+								id='bedrooms'
+								value={bedrooms}
+								onChange={onMutate}
+								min='1'
+								max='50'
+								required
+							/>
+						</div>
+						<div>
+							<label className='formLabel'>Bathrooms</label>
+							<input
+								className='formInputSmall'
+								type='number'
+								id='bathrooms'
+								value={bathrooms}
+								onChange={onMutate}
+								min='1'
+								max='50'
+								required
+							/>
+						</div>
+					</div>
+
+					<label className='formLabel'>Parking spot</label>
+					<div className='formButtons'>
+						<button
+							className={parking ? 'formButtonActive' : 'formButton'}
+							type='button'
+							id='parking'
+							value={true}
+							onClick={onMutate}
+							min='1'
+							max='50'
+						>
+							Yes
+						</button>
+						<button
+							className={
+								!parking && parking !== null ? 'formButtonActive' : 'formButton'
+							}
+							type='button'
+							id='parking'
+							value={false}
+							onClick={onMutate}
+						>
+							No
+						</button>
+					</div>
+
+					<label className='formLabel'>Furnished</label>
+					<div className='formButtons'>
+						<button
+							className={furnished ? 'formButtonActive' : 'formButton'}
+							type='button'
+							id='furnished'
+							value={true}
+							onClick={onMutate}
+						>
+                            Yes
+                        </button>
+						<button
+							className={!furnished && furnished !== null ? 'formButtonActive' : 'formButton'}
+							type='button'
+							id='furnished'
+							value={false}
+							onClick={onMutate}
+						>
+                            No
+                        </button>
+					</div>
+
+				</form>
+			</main>
+		</div>
+	)
 }
 
 export default CreateListing
