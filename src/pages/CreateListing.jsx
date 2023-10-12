00d492fa1d77f6a822d49f2eee1d2d60490a336e
unknown <shanelwilliams2018@gmail.com>
2023-10-12 00:01:58 -0600

    added geolocation

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -82,16 +82,25 @@ function CreateListing() {
 		let location
 
 		if(geolocationEnabled) {
-			const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyB2fYQoDwEfnZHFX06UsvQBGsnryugIrpo`)
+			const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
 
 			const data = await response.json()
 
-			console.log(data)
+			geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+			geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
 
+			location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address
+
+			if(location === undefined || location.includes('undefined')) {
+				setLoading(false)
+				toast.error('Please enter a correct address')
+				return
+			}
 		} else {
 			geolocation.lat = latitude
 			geolocation.lng = longitude
 			location = address
+			
 		}
 
 		setLoading(false)
